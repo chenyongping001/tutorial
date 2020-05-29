5a7e5da7278ef6e753f3ffc72e0d1b679c63a317
cyp <chenyongping001@qq.com>
2020-05-29 15:12:06 +0800

    Relationships & Hyperlinked APIs

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics
+from rest_framework import generics, renderers
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
 
@@ -29,3 +32,20 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response(
+        {
+            'users': reverse('user-list', request=request, format=format),
+            'snippets': reverse('snippet-list', request=request, format=format)
+        }
+    )
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
